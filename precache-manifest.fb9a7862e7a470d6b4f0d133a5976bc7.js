a9e9ad9545ffdc78ee600a446dd2a769abe605b3
Todd M <tboogie87@me.com>
2020-05-23 09:36:33 -0400

    Updates

---
+++ precache-manifest.fb9a7862e7a470d6b4f0d133a5976bc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7806c51436397b23480a4e543e31531c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9b8a79235cd0363e07e1",
+    "url": "/monsters-rolodex/static/css/main.df561760.chunk.css"
+  },
+  {
+    "revision": "8a6eda421bc5765c55c2",
+    "url": "/monsters-rolodex/static/js/2.bc65f737.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.bc65f737.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b8a79235cd0363e07e1",
+    "url": "/monsters-rolodex/static/js/main.87513ca0.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
